31677c849618da2445e636ea71325a031ff30972
aaronspence101 <32298150+aaronspence101@users.noreply.github.com>
2017-09-26 12:36:39 +0100

    Add files via upload

---
+++ best code yet.py
@@ -0,0 +1,28 @@
+print("                    This is a genral knowledge quiz")
+
+playername = input("What is your name: ")
+playerage = input("What is your age: ")
+text_file = open("Info_Players.txt", "w")
+
+text_file.write("PlayerOne")
+text_file.write("\n         Name : " + playername)
+text_file.write("\n          Age : " + playerage)
+
+text_file.close()
+
+
+
+print (" Answer with the chioices a,b or c ")
+print(" Make sure you keep your Caps lock off")
+print(" Here are 3 diffrent topics to choose from")
+print("     1.  Football")
+print("     2.  Music")
+print("     3.  General Knowledge")
+Menu = input ("Make your Choice [1-3]: ")
+
+
+level = input("Easy, Medium or Hard?: ")
+
+if level in ['easy','Easy']:
+    import footballeasy
+
